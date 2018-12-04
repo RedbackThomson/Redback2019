eb06db6ca4ea1b50fec95c5265d0557bdaa8e453
Redback <thomsonnicholas@hotmail.com>
2018-12-03 16:03:40 -0800

    Fixed PORT Env

--- express.js
+++ express.js
@@ -1,22 +1,23 @@
-const express = require('express');
-const throng = require('throng');
-
-const sourceDir = 'dist';
-const PORT = 3000;
-const WORKERS = process.env.WEB_CONCURRENCY || 1;
-
-throng({
-  workers: WORKERS,
-  lifetime: Infinity
-}, start);
-
-function start() {
-  var app = express();
-  app.use(express.static(sourceDir));
+(function () {
+  const express = require('express');
+  const throng = require('throng');
   
-  app.listen(PORT, () => {
-    console.log(`Express web server started: http://localhost:${PORT}`);
-    console.log(`Serving content from /${sourceDir}/`);
-  });
-}
-
+  const sourceDir = 'dist';
+  const PORT = process.env.PORT || 3000;
+  const WORKERS = process.env.WEB_CONCURRENCY || 1;
+  
+  throng({
+    workers: WORKERS,
+    lifetime: Infinity
+  }, start);
+  
+  function start() {
+    var app = express();
+    app.use(express.static(sourceDir));
+       
+    http.createServer(app).listen(app.get(PORT), function(){
+      console.log(`Express server listening on port ${PORT} on ${WORKERS} worker(s)`);
+      console.log(`Serving content from /${sourceDir}/`);
+    });
+  }
+}).call(this);  
